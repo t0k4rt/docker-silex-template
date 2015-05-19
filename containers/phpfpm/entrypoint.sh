3b3712bd0f41e9d811606cbd90124b5ef514fe8d
t0k4rt <alexandre.assouad@gmail.com>
2015-05-19 12:52:55 +0000

    updated entrypoint.sh script

--- containers/phpfpm/entrypoint.sh
+++ containers/phpfpm/entrypoint.sh
@@ -2,14 +2,13 @@
 set -e 
 shopt -s nocasematch
 
-TODO=""
-echo "command $@"
+echo "Running command: $@"
 
 
 if [ "$1" == "setup" ] && [ "$2" == "sf2" ]; then 
     cd /var/www
     if  [ -d "/var/www/symfony/web" ]; then
-        echo "symfony project already exists";
+        printf "\x1b[93;41m symfony project already exists \x1b[0m \n";
         exit 0
     fi
     echo $SF2_VERSION
@@ -17,6 +16,14 @@ if [ "$1" == "setup" ] && [ "$2" == "sf2" ]; then
     echo $@ 
     git clone -b $SF2_VERSION --depth 1 $SF2_REPO symfony
     cd symfony
+    cp app/config/parameters.yml.dist app/config/parameters.yml
+    UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    sed -r -e "s|(^.*database_host:).*$|\1 $POSTGRES_HOST|;\
+        s|(^.*database_port:).*$|\1 $POSTGRES_PORT|;\
+        s|(^.*database_user:).*$|\1 $POSTGRES_USER|;\
+        s|(^.*database_password:).*$|\1 $POSTGRES_PASSWORD|;\
+        s|(^.*secret:).*$|\1 $UUID|;" app/config/parameters.yml
+    
     composer install
     rm -rf app/cache/*
     php app/console assets:install --symlink web/ 
@@ -34,13 +41,10 @@ elif [ "$1" == "update" ] && [ "$2" == "sf2" ] && [ -d "/var/www/symfony/web" ];
     php app/console c:w 
     chown -R www-data:www-data /var/www/symfony
     exit 0
-elif php /var/www/symfony/app/console list|grep $1; then
-	echo "found command"; 
+elif [[ $1 == *":"* ]] && php /var/www/symfony/app/console list|grep $1; then
+	echo "Found command(s)"; 
     PREFIX="php /var/www/symfony/app/console " 
-    TODO=$PREFIX$@
-    #exec gosu root bash -c "ls  -l"
-    exec gosu www-data bash -c "$TODO"
+    exec gosu www-data bash -c "$PREFIX$@"
     exit 0    
 fi
-
 exec $@
