97dd1eb32f7f594508c8e59e4885b216b6d06563
t0k4rt <alexandre.assouad@gmail.com>
2015-05-22 17:05:56 +0000

    plugged errors to stderr / stdout

--- containers/phpfpm/entrypoint.sh
+++ containers/phpfpm/entrypoint.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 set -e 
-shopt -s nocasematch
+shopt -s nocasematch 
 
+composer2="hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 /usr/local/bin/composer"
 
+alias 
 if [ "$1" == "setup" ] && [ "$2" == "silex" ]; then 
     cd /var/www/
     git clone https://github.com/t0k4rt/silex-startup-template.git silex
     cd silex
-    composer install
+    composer install -vvvvv
     chown -R www-data:www-data /var/www/silex
     exit 0
 elif [ "$1" == "update" ] && [ "$2" == "silex" ]; then 
     cd /var/www/silex
     git pull origin master
-    composer update
+    exec $composer2 install -vvvvv
+    chown -R www-data:www-data /var/www/silex
+    exit 0
+elif [ "$1" == "update" ] && [ "$2" == "code" ]; then 
+    cd /var/www/silex
+    git pull origin master
+    chown -R www-data:www-data /var/www/silex
+    exit 0
+elif [ "$1" == "composer" ]; then 
+    cd /var/www/silex
+    exec $composer2 $2 -vvvvv
     chown -R www-data:www-data /var/www/silex
     exit 0
 fi
     
-echo "Running command: $@"
-exec $@
+
+key=$1
+shift
+echo "Running command: $key $@"
+exec $key $@
