4a9f23e061db77e03234e0dc5eaaa0e21182925c
t0k4rt <alexandre.assouad@gmail.com>
2015-05-25 16:25:26 +0000

    improved things

--- containers/phpfpm/entrypoint.sh
+++ containers/phpfpm/entrypoint.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 set -e 
-shopt -s nocasematch
+shopt -s nocasematch 
 
+if [ -z "$REPO" ]; then 
+    REPO="https://github.com/t0k4rt/silex-startup-template.git"
+fi
 
+WWW="/var/www"
+APP_DIR="app"
+APP_DIRPATH="$WWW/$APP_DIR"
+
+alias 
 if [ "$1" == "setup" ] && [ "$2" == "silex" ]; then 
-    cd /var/www/
-    git clone https://github.com/t0k4rt/silex-startup-template.git silex
-    cd silex
-    composer install
-    chown -R www-data:www-data /var/www/silex
+    cd $WWW
+    if  [ -f "$APP_DIR/composer.json" ]; then
+        printf "\x1b[93;41m Silex project already exists \x1b[0m \n";
+        exit 0
+    fi
+    git clone $REPO $APP_DIR
+    cd $APP_DIRPATH
+    composer install -vvvvv
+    chown -R www-data:www-data $APP_DIRPATH
     exit 0
 elif [ "$1" == "update" ] && [ "$2" == "silex" ]; then 
-    cd /var/www/silex
+    cd $APP_DIRPATH
     git pull origin master
-    composer update
-    chown -R www-data:www-data /var/www/silex
+    composer install -vvvvv
+    chown -R www-data:www-data $APP_DIRPATH
+    exit 0
+elif [ "$1" == "update" ] && [ "$2" == "code" ]; then 
+    cd $APP_DIRPATH
+    git pull origin master
+    chown -R www-data:www-data $APP_DIRPATH
+    exit 0
+elif [ "$1" == "composer" ]; then 
+    cd $APP_DIRPATH
+    shift
+    composer $@ -vvvvv
+    chown -R www-data:www-data $APP_DIRPATH
     exit 0
 fi
     
-echo "Running command: $@"
-exec $@
+
+key=$1
+shift
+echo "Running command: $key $@"
+exec $key $@
